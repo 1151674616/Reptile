0833492d058fc57e312fd7354d4227ff73fc1e1f
肆风 <30339213+1151674616@users.noreply.github.com>
2019-04-09 23:05:18 +0800

    Imooc-Ad-maste

---
+++ Imooc-Ad-master/imooc-ad/ad-eureka/src/main/java/org/ad/eureka/App.java
@@ -0,0 +1,13 @@
+package org.ad.eureka;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
