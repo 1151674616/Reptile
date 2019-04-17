2be862fe4935a54f4108eca5c80f6210d759df69
1151674616 <1151674616@qq.com>
2019-04-17 17:03:14 +0800

    第五章还差网关配置

--- Imooc-Ad-master/imooc-ad/imooc-ad-service/ad-sponsor/src/main/java/com/imooc/ad/SponsorApplication.java
+++ Imooc-Ad-master/imooc-ad/imooc-ad-service/ad-sponsor/src/main/java/com/imooc/ad/SponsorApplication.java
@@ -2,6 +2,7 @@ package com.imooc.ad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -12,7 +13,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 @EnableCircuitBreaker
 @EnableEurekaClient
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class SponsorApplication {
 
     public static void main(String[] args) {
